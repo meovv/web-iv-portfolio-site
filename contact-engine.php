0ab99db86db251d4f1de6de460dd0923907c5209
meovv <b0:ab:83:79:50:8c:c8:23:b7:46:aa:8a:3e:92:1f:d7>
2015-06-02 19:02:00 -0500

    Alignment and Spacing issues
    Fixed some alignment and spacing issues within the homepage, and
    project pages.

---
+++ contact-engine.php
@@ -0,0 +1,40 @@
+<?php
+
+$EmailFrom = "form@morhdesign.com";
+$EmailTo = "info@morhdesign.com";
+$Subject = "Contact Form Submission from morhdesign.com";
+$Name = Trim(stripslashes($_POST['Name'])); 
+$Tel = Trim(stripslashes($_POST['Tel'])); 
+$Email = Trim(stripslashes($_POST['Email'])); 
+$Message = Trim(stripslashes($_POST['Message'])); 
+
+// validation
+$validationOK=true;
+if (!$validationOK) {
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=error.htm\">";
+  exit;
+}
+
+// prepare email body text
+$Body = "";
+$Body .= "Name: ";
+$Body .= $Name;
+$Body .= "\n";
+$Body .= "Email: ";
+$Body .= $Email;
+$Body .= "\n";
+$Body .= "Message: ";
+$Body .= $Message;
+$Body .= "\n";
+
+// send email 
+$success = mail($EmailTo, $Subject, $Body, "From: <$EmailFrom>");
+
+// redirect to success page 
+if ($success){
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=contact-thanks.php\">";
+}
+else{
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=error.htm\">";
+}
+?>
---
+++ contact-thanks.php
@@ -0,0 +1,34 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Strict//EN">
+
+<head>
+	<title>Message was sent!</title>
+	<script src="scripts/jquery-2.1.4.min.js"></script>
+      <link rel="stylesheet" type="text/css" href="styles/foundation.css">
+      <link rel="stylesheet" type="text/css" href="styles/foundation.min.css">
+      <link rel="stylesheet" type="text/css" href="styles/main.css">
+</head>
+
+<body>
+	<div class="row">
+    	<div class="small-centered small-12 medium-5 columns">
+        	<h1>message sent!</h1>
+        </div>
+   </div>
+   
+   <div class="row">
+   	<div class="small-centered small-12 medium-6 columns">
+    	<p>Thank you for your message! I'll be sure to respond to you as soon as possible.</p>
+    </div>
+  </div>
+   
+   <div class="row">
+    <div class="small-centered small-3 columns">
+		
+		<a href="index.html">back to morhdesign.com</a>
+	
+	</div>
+   </div>
+
+</body>
+
+</html>
